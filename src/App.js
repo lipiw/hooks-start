7967a376048913a546a2325ae235c85db2a07517
Felipe Rafael Goncalves <fgoncalves@cpqd.com>
2022-01-28 22:30:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
-function App() {
+export default function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  //useEffect recebe dois parametros, 1) oq ele ira fazer, 2) quando ele ira fazer. (array vazio representa apenas uma vez quando o componente for carregado)
+  
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/lipiw/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
+  useEffect(() =>{
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`
+  }, [repositories]);
+
+  function handleFavorite(id){
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite ? <span>(Favorito)</span> : <span></span>}
+          <button onClick={() => handleFavorite(repo.id)}>ADICIONAR</button>
+          </li>
+      ))}
+    </ul>
   );
 }
 
-export default App;
